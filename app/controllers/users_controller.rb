318a2203a721688c24cad10a9c2d8e27e4d7fce3
attenbl19 <attenboroughl@protonmail.com>
2020-10-01 13:44:47 -0400

    working on actions for user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,13 +15,18 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
+    @user = User.create(user_params)
+  
+    if @user.valid?
+      render json: @user
     end
+  
+    
+    # if @user.save
+    #   render json: @user, status: :created, location: @user
+    # else
+    #   render json: @user.errors, status: :unprocessable_entity
+    # end
   end
 
   # PATCH/PUT /users/1
@@ -46,6 +51,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name)
+      params.permit(:name)
     end
 end
